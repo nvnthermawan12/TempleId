997ce329a63ac28a949a1ef0623e999fc5ce20a2
@nvnthermawan12 <nvnthermawan@gmail.com>
2021-07-27 20:08:10 +0700

    first commit

---
+++ TempleId/TempleIdApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempleIdApp.swift
+//  TempleId
+//
+//  Created by Novianto Hermawan on 27/07/21.
+//
+
+import SwiftUI
+
+@main
+struct TempleIdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
